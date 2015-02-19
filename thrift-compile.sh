e225a1481fbf301284a489f81e9325d8273f9c3f
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2015-02-19 16:37:22 +0100

    Move dozmod/sat thrift RPC to this lib

--- thrift-compile.sh
+++ thrift-compile.sh
@@ -9,13 +9,28 @@ if ! thrift --version | grep -q "0\.9\.1"; then
 	fi
 	echo "OK, compiling..."
 fi
+
+echo "1) Masterserver RPC"
 [ -e gen-java ] && rm -r gen-java
 if thrift --gen java src/main/thrift/imagemaster.thrift; then
 	[ -e "src/main/java/org/openslx/imagemaster/thrift/iface" ] && rm -rf src/main/java/org/openslx/imagemaster/thrift/iface
-	cp -r gen-java/org src/main/java/ && echo "Success." && exit 0
+	if ! cp -r gen-java/org src/main/java/; then
+		echo "Error copying compiled files! Aborting!"
+		exit 1
+	fi
 fi
 
-echo "Error!"
-exit 1
+echo "2) Sat RPC"
+[ -e gen-java ] && rm -r gen-java
+if thrift --gen java src/main/thrift/sat.thrift; then
+	[ -e "src/main/java/org/openslx/sat/thrift/iface" ] && rm -rf src/main/java/org/openslx/sat/thrift/iface
+	if ! cp -r gen-java/org src/main/java/; then
+		echo "Error copying compiled files! Aborting!"
+		exit 1
+	fi
+fi
+
+echo "All done!"
+exit 0
 
 
