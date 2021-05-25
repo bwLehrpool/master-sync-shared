4f4ff072689e6d780d7ba0cc9a3ae19f2ec9db73
Manuel Bentele <development@manuel-bentele.de>
2021-05-25 09:06:24 +0200

    Add API method to obtain Libvirt test resource from test *.jar file

--- src/test/java/org/openslx/libvirt/xml/LibvirtXmlTestResources.java
+++ src/test/java/org/openslx/libvirt/xml/LibvirtXmlTestResources.java
@@ -2,6 +2,7 @@ package org.openslx.libvirt.xml;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 public final class LibvirtXmlTestResources
@@ -17,6 +18,12 @@ public final class LibvirtXmlTestResources
 		String libvirtXmlPath = LibvirtXmlTestResources.LIBVIRT_PREFIX_PATH_XML + File.separator + libvirtXmlFileName;
 		URL libvirtXml = LibvirtXmlTestResources.class.getResource( libvirtXmlPath );
 		return new File( libvirtXml.getFile() );
+	}
+
+	public static InputStream getLibvirtXmlStream( String libvirtXmlFileName )
+	{
+		String libvirtXmlPath = LibvirtXmlTestResources.LIBVIRT_PREFIX_PATH_XML + File.separator + libvirtXmlFileName;
+		return LibvirtXmlTestResources.class.getResourceAsStream( libvirtXmlPath );
 	}
 
 	public static File createLibvirtXmlTempFile() throws IOException
