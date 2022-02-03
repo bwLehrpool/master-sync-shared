d81bd1875d6eae52befbe98488d569402a57a975
Manuel Bentele <development@manuel-bentele.de>
2022-02-03 12:13:18 +0100

    Fix resource path for virtualizer config tests on Windows platforms

--- src/test/java/org/openslx/virtualization/configuration/logic/ConfigurationLogicTestResources.java
+++ src/test/java/org/openslx/virtualization/configuration/logic/ConfigurationLogicTestResources.java
@@ -3,17 +3,19 @@ package org.openslx.virtualization.configuration.logic;
 import java.io.File;
 import java.net.URL;
 
+import org.openslx.util.Resources;
+
 public class ConfigurationLogicTestResources
 {
-	private static final String VMWARE_PREFIX_PATH = File.separator + "vmware";
-	private static final String VMWARE_PREFIX_PATH_VMX = VMWARE_PREFIX_PATH + File.separator + "vmx";
+	private static final String VMWARE_PREFIX_PATH = Resources.PATH_SEPARATOR + "vmware";
+	private static final String VMWARE_PREFIX_PATH_VMX = VMWARE_PREFIX_PATH + Resources.PATH_SEPARATOR + "vmx";
 
-	private static final String VIRTUALBOX_PREFIX_PATH = File.separator + "virtualbox";
-	private static final String VIRTUALBOX_PREFIX_PATH_XML = VIRTUALBOX_PREFIX_PATH + File.separator + "xml";
+	private static final String VIRTUALBOX_PREFIX_PATH = Resources.PATH_SEPARATOR + "virtualbox";
+	private static final String VIRTUALBOX_PREFIX_PATH_XML = VIRTUALBOX_PREFIX_PATH + Resources.PATH_SEPARATOR + "xml";
 
 	private static File getFile( String prefixPath, String fileName )
 	{
-		final String filePath = prefixPath + File.separator + fileName;
+		final String filePath = prefixPath + Resources.PATH_SEPARATOR + fileName;
 		final URL fileUrl = ConfigurationLogicTestResources.class.getResource( filePath );
 		return new File( fileUrl.getFile() );
 	}
