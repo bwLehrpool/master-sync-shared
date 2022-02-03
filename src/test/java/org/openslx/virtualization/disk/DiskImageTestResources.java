1d999c8f6b3304432512028a9a943e4b55ccf92f
Manuel Bentele <development@manuel-bentele.de>
2022-02-03 12:07:14 +0100

    Fix resource path for disk tests on Windows platforms

--- src/test/java/org/openslx/virtualization/disk/DiskImageTestResources.java
+++ src/test/java/org/openslx/virtualization/disk/DiskImageTestResources.java
@@ -3,13 +3,15 @@ package org.openslx.virtualization.disk;
 import java.io.File;
 import java.net.URL;
 
+import org.openslx.util.Resources;
+
 public class DiskImageTestResources
 {
-	private static final String DISK_PREFIX_PATH = File.separator + "disk";
+	private static final String DISK_PREFIX_PATH = Resources.PATH_SEPARATOR + "disk";
 
 	public static File getDiskFile( String diskFileName )
 	{
-		String diskPath = DiskImageTestResources.DISK_PREFIX_PATH + File.separator + diskFileName;
+		String diskPath = DiskImageTestResources.DISK_PREFIX_PATH + Resources.PATH_SEPARATOR + diskFileName;
 		URL disk = DiskImageTestResources.class.getResource( diskPath );
 		return new File( disk.getFile() );
 	}
